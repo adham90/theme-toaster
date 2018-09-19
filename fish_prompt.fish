4829368c27008abc8f49fe83d145cdc652931fc6
adham90 <adham.eldeeb90@gmail.com>
2018-09-19 13:01:43 +0200

    edit git prompt and change colors

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -31,35 +31,15 @@ function __toaster_git_branch_name
   echo (git rev-parse --abbrev-ref HEAD ^/dev/null)
 end
 
-function __toaster_rainbow
-  if echo $argv[1] | grep -q -e $argv[3]
-    __toaster_color_echo $argv[2] "彡ミ"
-  end
-end
-
-function __toaster_git_status_icons
-  set -l git_status (__toaster_git_status_codes)
-
-  __toaster_rainbow $git_status $__toaster_color_pink 'D'
-  __toaster_rainbow $git_status $__toaster_color_orange 'R'
-  __toaster_rainbow $git_status $__toaster_color_white 'C'
-  __toaster_rainbow $git_status $__toaster_color_green 'A'
-  __toaster_rainbow $git_status $__toaster_color_blue 'U'
-  __toaster_rainbow $git_status $__toaster_color_lilac 'M'
-  __toaster_rainbow $git_status $__toaster_color_grey '?'
-end
-
 function __toaster_git_status
   # In git
   if test -n (__toaster_git_branch_name)
 
-    __toaster_color_echo $__toaster_color_blue " git"
-    __toaster_color_echo $__toaster_color_white ":"(__toaster_git_branch_name)
+    __toaster_color_echo $__toaster_color_blue "::"
+    __toaster_color_echo $__toaster_color_blue (__toaster_git_branch_name)
 
     if test -n (__toaster_git_status_codes)
-      __toaster_color_echo $__toaster_color_pink ' ●'
-      __toaster_color_echo $__toaster_color_white ' [^._.^]ﾉ'
-      __toaster_git_status_icons
+      __toaster_color_echo $__toaster_color_yellow ' ●'
     else
       __toaster_color_echo $__toaster_color_green ' ○'
     end
@@ -71,5 +51,5 @@ function fish_prompt
   __toaster_color_echo $__toaster_color_purple (__toaster_current_folder)
   __toaster_git_status
   echo
-  __toaster_color_echo $__toaster_color_pink "\$ "
+  __toaster_color_echo $__toaster_color_white "\$ "
 end
